31c3d7b540e52cbd85e680f3e808af5babffc8eb
Mohd Hafiz <xmhafiz@gmail.com>
2021-06-29 08:38:58 +0800

    Added service and viewModel

--- InfinitListSwiftUI/APIService.swift
+++ InfinitListSwiftUI/APIService.swift
@@ -1,1 +1,34 @@
+//
+//  UserViewModel.swift
+//  InfinitListSwiftUI
+//
+//  Created by Hafiz on 28/06/2021.
+//
 
+import Foundation
+import Combine
+
+class UserViewModel: ObservableObject {
+    @Published var users: [User] = []
+    @Published var isRequestFailed = false
+    private let pageLimit = 25
+    private var currentLastId: Int? = nil
+    private var cancellable: AnyCancellable?
+    
+    func getUsers() {
+        cancellable = APIService.getUsers(perPage: pageLimit, sinceId: currentLastId)
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    self.isRequestFailed = true
+                    print(error)
+                case .finished:
+                    print("finished")
+                }
+            } receiveValue: { users in
+                self.users.append(contentsOf: users)
+                self.currentLastId = users.last?.id
+            }
+    }
+}
--- InfinitListSwiftUI/UserViewModel.swift
+++ InfinitListSwiftUI/UserViewModel.swift
@@ -1,1 +1,25 @@
+//
+//  APIService.swift
+//  InfinitListSwiftUI
+//
+//  Created by Hafiz on 28/06/2021.
+//
 
+import Foundation
+import Combine
+
+class APIService {
+    static func getUsers(perPage: Int = 30, sinceId: Int? = nil) -> AnyPublisher<[User], Error> {
+        var components = URLComponents(string: "https://api.github.com/users")!
+        components.queryItems = [
+            URLQueryItem(name: "per_page", value: "\(perPage)"),
+            URLQueryItem(name: "since", value: (sinceId != nil) ? "\(sinceId!)" : nil)
+        ]
+        
+        let request = URLRequest(url: components.url!, timeoutInterval: 5)
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .map(\.data)
+            .decode(type: [User].self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
