9f1393967bf3ed0fa10b8cf71130e99d25555742
Mohd Hafiz <xmhafiz@gmail.com>
2021-06-29 08:33:32 +0800

    init project

---
+++ InfinitListSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InfinitListSwiftUI
+//
+//  Created by Hafiz on 27/06/2021.
+//
+import Combine
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello world!")
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InfinitListSwiftUI/InfinitListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfinitListSwiftUIApp.swift
+//  InfinitListSwiftUI
+//
+//  Created by Hafiz on 27/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct InfinitListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
