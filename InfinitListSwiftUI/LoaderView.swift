016158f68cedfb99b33c037fdb1642080f94cfe2
Mohd Hafiz <xmhafiz@gmail.com>
2021-06-29 08:38:19 +0800

    Added rowView and loaderView

--- InfinitListSwiftUI/LoaderView.swift
+++ InfinitListSwiftUI/LoaderView.swift
@@ -1,1 +1,24 @@
+//
+//  LoaderView.swift
+//  InfinitListSwiftUI
+//
+//  Created by Hafiz on 27/06/2021.
+//
+
+import SwiftUI
+
+struct LoaderView: View {
+    @Binding var isFailed: Bool
+    var body: some View {
+        Text(isFailed ? "Failed. Tap to retry." : "Loading..")
+            .foregroundColor(isFailed ? .red : .green)
+            .padding()
+    }
+}
+
+struct LoaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoaderView(isFailed: .constant(false))
+    }
+}
 
--- InfinitListSwiftUI/UserRow.swift
+++ InfinitListSwiftUI/UserRow.swift
@@ -1,1 +1,31 @@
 
+//
+//  UserRow.swift
+//  InfinitListSwiftUI
+//
+//  Created by Hafiz on 27/06/2021.
+//
+
+import SwiftUI
+
+struct UserRow: View {
+    let user: User
+    var body: some View {
+        HStack (spacing: 12) {
+            Image("imagePlaceholder")
+                .resizable()
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+            Text(user.name)
+        }
+        .padding(4)
+        
+    }
+}
+
+struct UserRow_Previews: PreviewProvider {
+    static var previews: some View {
+        let mockUser = User(id: 1, name: "John Doe", avatarUrl: "")
+        UserRow(user: mockUser)
+    }
+}
